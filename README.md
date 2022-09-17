3ba0cc52b4745ff2058c2824fd035526a7f5a6b8
Naveen Tammareddy <tammanaveen@gmail.com>
2022-09-17 22:43:55 +0530

    Edit button added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # populate-html-elements-from-json-dynamically
-populate html elements from json response dynamically. react project. 
+populate html elements from json response dynamically. this is react project. 
+
+
+![App Screenshot](htmlFromJson.png)
--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { htmlElements } from './htmlElements';
 import { mockData } from './mockData';
 
 function App() {
+
+ const editBtnFunc = (rowData) => {
+  alert(JSON.stringify(rowData))
+ }
+
   return (
     <div className="container">
       <h1>Populate HTML elements from JSON response dynamically</h1>
@@ -16,10 +21,11 @@ function App() {
         </thead>
         <tbody>
           {mockData.map((jsonObj, jsonIndex) => (
-              <tr>
+              <tr key={jsonIndex}>
                 {htmlElements.map((htmlElement, eleIndex) => (
                   <td key={eleIndex}>{htmlElement.getHtmlElement(jsonObj)}</td>
                 ))}
+                <button className="btn btn-warning" onClick={() => editBtnFunc(jsonObj)}>Edit</button>
               </tr>
             ))
           }
